d8494e75881c154ec4f080908d5546ffd253658f
Merus23 <mateus.satira@gmail.com>
2025-02-11 12:51:56 -0300

    chore: adiciona front e back ao projeto

---
+++ BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/EntrevistaPlanisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.entrevistaPlanisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrevistaPlanisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrevistaPlanisaApplication.class, args);
+	}
+
+}
