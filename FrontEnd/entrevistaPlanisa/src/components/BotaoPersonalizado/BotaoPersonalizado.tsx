991be46e968908e93849108fcc23f39ab5c69fee
Merus23 <mateus.satira@gmail.com>
2025-02-12 17:32:36 -0300

    feat: adiciona a possibilidade de desativar o botão personalizado

--- FrontEnd/entrevistaPlanisa/src/components/BotaoPersonalizado/BotaoPersonalizado.tsx
+++ FrontEnd/entrevistaPlanisa/src/components/BotaoPersonalizado/BotaoPersonalizado.tsx
@@ -1,15 +1,22 @@
 export interface ButaoPersonalizadoInterface {
   nome: string;
   aoClicar: () => void;
+  desabilitar: boolean;
 }
 
 export default function BotaoPersonalizado({
   nome,
   aoClicar,
+  desabilitar,
 }: ButaoPersonalizadoInterface) {
   return (
     <button
-      className="border-2 rounded-lg p-2 bg-[#12123A] text-white hover:font-bold hover:cursor-pointer"
+      disabled={desabilitar}
+      className={`${
+        desabilitar
+          ? "bg-gray-800 border-gray-800 cursor-not-allowed"
+          : "border-[#12123A] bg-[#12123A] hover:cursor-pointer hover:font-bold"
+      } border-2  rounded-lg p-2 text-white  `}
       onClick={aoClicar}
       id={`id_${nome.replace(/\s+/g, "")}`}
     >
