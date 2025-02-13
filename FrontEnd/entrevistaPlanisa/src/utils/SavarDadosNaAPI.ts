f948e8fb6cf4f008bf907552d2b3937ef55e1527
Merus23 <mateus.satira@gmail.com>
2025-02-13 15:10:55 -0300

    feat: adiciona uma funçã para salvar os dados na api

---
+++ FrontEnd/entrevistaPlanisa/src/utils/SavarDadosNaAPI.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+interface Benchmark {
+  titulo: string;
+  pais1: string;
+  pais2: string;
+  tipoComparacao: string;
+  dataInicio: string;
+  dataTermino: string;
+}
+/**
+ * @description Salva um novo benchmark no banco de dados através da API.
+ * @param benchmark Um objeto `Benchmark` contendo os dados a serem salvos.
+ * @returns Uma Promise que resolve para o objeto `Benchmark` retornado pela API após o salvamento,
+ *          ou `undefined` se ocorrer um erro durante o processo.
+ * @throws Se a requisição à API falhar, um erro será lançado.  (Nota: atualmente, o erro é apenas logado no console, seria melhor relançá-lo para tratamento no componente)
+ */
+export async function SalvarBenchmark(benchmark: Benchmark) {
+  const url = import.meta.env.VITE_URL_BASE;
+  console.log("BENCHMARK", benchmark);
+  try {
+    const response = await axios.post(`${url}benchmarks/`, benchmark, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Benchmark salvo com sucesso:", response.data);
+    return response.data;
+  } catch (error: any) {
+    console.error("Erro ao salvar benchmark:", error);
+  }
+}
