1da73b616ad026fad023da38950ee50bc09c75ca
Merus23 <mateus.satira@gmail.com>
2025-02-13 16:10:54 -0300

    chore: adiciona docstring na função FormataData

--- FrontEnd/entrevistaPlanisa/src/utils/FormataData.ts
+++ FrontEnd/entrevistaPlanisa/src/utils/FormataData.ts
@@ -1,3 +1,8 @@
+/**
+ * @description Função que formata a data no formato dd/mm/aaaa
+ * @param {string} data - Data a ser formatada
+ * @returns {string} - Data formatada
+ */
 export function FormataData(data: string): string {
   const dataFormatada = new Date(data);
   return dataFormatada.toLocaleDateString();
