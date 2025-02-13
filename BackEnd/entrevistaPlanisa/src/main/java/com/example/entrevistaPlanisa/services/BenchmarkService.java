23e30527f6642ad356d1eb8a1b541d04c81790c8
Merus23 <mateus.satira@gmail.com>
2025-02-12 22:15:37 -0300

    feat: adiciona a função criar benchmark, no benchmark service

--- BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/services/BenchmarkService.java
+++ BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/services/BenchmarkService.java
@@ -1,7 +1,30 @@
 package com.example.entrevistaPlanisa.services;
 
+import com.example.entrevistaPlanisa.dtos.CreateBenchmarkDTO;
+import com.example.entrevistaPlanisa.models.Benchmark;
+import com.example.entrevistaPlanisa.repositories.BenchmarkRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class BenchmarkService {
+
+    private final BenchmarkRepository benchmarkRepository;
+
+    public BenchmarkService(BenchmarkRepository benchmarkRepository) {
+        this.benchmarkRepository = benchmarkRepository;
+    }
+
+    public Benchmark criar(CreateBenchmarkDTO benchmark) {
+        Benchmark entity = new Benchmark();
+
+        entity.setTitulo(benchmark.titulo());
+        entity.setPais1(benchmark.pais1());
+        entity.setPais2(benchmark.pais2());
+        entity.setTipoComparacao(benchmark.tipoComparacao());
+        entity.setDataInicio(benchmark.dataInicio());
+        entity.setDataTermino(benchmark.dataTermino());
+
+        return this.benchmarkRepository.save(entity);
+    }
 }
