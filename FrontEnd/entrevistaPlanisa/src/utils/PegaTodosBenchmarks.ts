f05f4fc9dccfb28648fb5c65763b4ec64dfe4453
Merus23 <mateus.satira@gmail.com>
2025-02-13 16:08:04 -0300

    feat: adiciona a função PegaTodosBenchmarks

---
+++ FrontEnd/entrevistaPlanisa/src/utils/PegaTodosBenchmarks.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { Benchmark } from "./SavarDadosNaAPI";
+
+export interface pegaBenchmark extends Benchmark {
+  id: number;
+}
+
+export async function PegaTodosBenchmarks() {
+  const url = import.meta.env.VITE_URL_BASE;
+  try {
+    const response = await axios.get(`${url}benchmarks/`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.data) {
+      return { benchmacks: response.data as pegaBenchmark[] };
+    }
+
+    return { benchmarks: [] };
+
+    console.log("Benchmarks", response.data);
+  } catch (error: any) {
+    console.error("Erro ao salvar benchmark:", error);
+  }
+}
