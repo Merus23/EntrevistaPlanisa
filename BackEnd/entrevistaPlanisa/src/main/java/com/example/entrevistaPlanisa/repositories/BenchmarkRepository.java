470020208a813795629d531a4c0bf3beb058dcd1
Merus23 <mateus.satira@gmail.com>
2025-02-13 11:40:38 -0300

    feat: adiciona funções para buscar informações na base de dados, no benchmark repository

--- BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/repositories/BenchmarkRepository.java
+++ BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/repositories/BenchmarkRepository.java
@@ -2,7 +2,21 @@ package com.example.entrevistaPlanisa.repositories;
 
 import com.example.entrevistaPlanisa.models.Benchmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 public interface BenchmarkRepository extends JpaRepository<Benchmark, Long> {
+
+    Optional<List<Benchmark>> findByPais1(String pais1);
+    Optional<List<Benchmark>> findByPais2(String pais2);
+    Optional<List<Benchmark>> findByTitulo(String titulo);
+    Optional<List<Benchmark>> findByTipoCompracao(String tipoComparacao);
+
+    @Query("SELECT b FROM Benchmark b WHERE b.dataInicio >= :startDate AND b.dataTermino <= :endDate")
+    Optional<List<Benchmark>> findByEntreDataInicioEDataTermino(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
 }
