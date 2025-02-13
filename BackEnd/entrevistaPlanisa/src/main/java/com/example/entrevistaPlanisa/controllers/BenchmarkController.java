46250b4adf8f9749891963f7166ada515cbd443f
Merus23 <mateus.satira@gmail.com>
2025-02-13 16:06:11 -0300

    feat: adiciona a cors para aceitar requisições do frontend

--- BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/controllers/BenchmarkController.java
+++ BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/controllers/BenchmarkController.java
@@ -6,15 +6,13 @@ import com.example.entrevistaPlanisa.services.BenchmarkService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/benchmarks")
+@CrossOrigin(origins = "http://localhost:5173")
 public class BenchmarkController {
 
     private final BenchmarkService benchmarkService;
