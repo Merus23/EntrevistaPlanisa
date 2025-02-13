348d596737fa2c753baf3dae8919086cf8bed80b
Merus23 <mateus.satira@gmail.com>
2025-02-13 13:02:51 -0300

    feat: adiciona novos métodos para lidar com busca de determinadas propriedades do benchmark

--- BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/services/BenchmarkService.java
+++ BackEnd/entrevistaPlanisa/src/main/java/com/example/entrevistaPlanisa/services/BenchmarkService.java
@@ -1,12 +1,12 @@
 package com.example.entrevistaPlanisa.services;
 
-import com.example.entrevistaPlanisa.dtos.CreateBenchmarkDTO;
+import com.example.entrevistaPlanisa.dtos.Requisicao.CreateBenchmarkDTO;
 import com.example.entrevistaPlanisa.models.Benchmark;
 import com.example.entrevistaPlanisa.repositories.BenchmarkRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BenchmarkService {
@@ -20,6 +20,28 @@ public class BenchmarkService {
     public List<Benchmark> listar() {
         return this.benchmarkRepository.findAll();
     }
+
+    public Optional<List<Benchmark>> listarPorPais1(String pais1) {
+        return this.benchmarkRepository.findByPais1(pais1);
+    }
+
+    public Optional<List<Benchmark>> listarPorPais2(String pais2) {
+        return this.benchmarkRepository.findByPais2(pais2);
+    }
+
+    public Optional<List<Benchmark>> listarPorTitulo(String titulo) {
+        return this.benchmarkRepository.findByTitulo(titulo);
+    }
+
+    public Optional<List<Benchmark>> listarPorTipoComparacao(String tipoComparacao) {
+        return this.benchmarkRepository.findByTipoComparacao(tipoComparacao);
+    }
+
+    public void deletar(Long id) {
+        this.benchmarkRepository.deleteById(id);
+    }
+
+
 
     public Benchmark criar(CreateBenchmarkDTO benchmark) {
         Benchmark entity = new Benchmark();
