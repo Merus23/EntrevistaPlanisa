efa2725b8ed8f557c26c226401e8f8adfa99ecc8
Merus23 <mateus.satira@gmail.com>
2025-02-14 00:21:07 -0300

    feat: adiciona uma função para atualizar os benchmarks

---
+++ FrontEnd/entrevistaPlanisa/src/utils/Benchmark/AtualizarBenchmark.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { pegaBenchmark } from "./PegaTodosBenchmarks";
+
+export async function AtualizarBenchmark(benchmark: pegaBenchmark) {
+  const url = import.meta.env.VITE_URL_BASE;
+  console.dir(benchmark);
+
+  try {
+    const response = await axios.put(`${url}benchmarks/`, benchmark, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.data) {
+      return { benchmark: response.data as pegaBenchmark };
+    }
+
+    return { benchmarks: [] };
+  } catch (error: any) {
+    console.error("Erro ao salvar benchmark:", error);
+  }
+}
